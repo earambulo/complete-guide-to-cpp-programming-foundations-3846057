f97cf191ced6418fa250db5d0fccc78070abf3ca
earambulo <44985612+earambulo@users.noreply.github.com>
2025-09-02 14:48:09 +0000

    This is a test

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Sebastian
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello!" << std::endl;
+    
+    std::cout << std::endl << std::endl;
+    return 0;
+}
